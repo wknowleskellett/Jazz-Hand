64d584bbace3fc5712cf6117daff2ab9e177b9b1
William Knowles-Kellett <39969985+wknowleskellett@users.noreply.github.com>
2022-05-07 19:43:21 +0100

    Clean up comments in Read_Fingies.ino

--- Read_Fingies/Read_Fingies.ino
+++ Read_Fingies/Read_Fingies.ino
@@ -3,21 +3,18 @@ int fsrReadings[5];
 
 
 void setup() {
-  // put your setup code here, to run once:
-  //pinMode(13, INPUT);
+  // Prepare to send readings to the computer over USB
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-//  int readValue = digitalRead(13);
-//  digitalWrite(8, 1-readValue);
-//  Serial.println(readValue);
+  // Iterate over each finger and output voltage readings, tab separated
   for (int i=0; i < 5; i++) {
     fsrReadings[i] = analogRead(FSRPIN[i]);
     Serial.print(fsrReadings[i]);
     Serial.print('\t');
   }
+  // Flush and prepare to send new data
   Serial.println();
   delay(100);
 }
